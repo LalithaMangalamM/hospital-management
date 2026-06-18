664dcf32763894bf24ec978183c46b4d08692290
LalithaMangalamM <lalli155003@gmail.com>
2026-06-18 16:09:51 +0530

    Create join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_06_18_000001) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_18_103841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_18_000001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
+  end
+
+  create_table "doctors_hospitals", id: false, force: :cascade do |t|
+    t.bigint "doctor_id", null: false
+    t.bigint "hospital_id", null: false
   end
 
   create_table "hospitals", force: :cascade do |t|
